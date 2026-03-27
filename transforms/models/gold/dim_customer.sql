9d85e41df2428f83241c52d8c117d8380145b07a
rizkyangga-tech <rizkyangga3008@gmail.com>
2026-03-27 09:09:39 +0800

    fix

--- transforms/models/gold/dim_customer.sql
+++ transforms/models/gold/dim_customer.sql
@@ -1,22 +1,9 @@
 {{ config(materialized='table') }}
 
 SELECT
-    c.customer_id,
-    c.name,
-    c.gender,
-    c.city,
-    c.birth_date,
-
-    DATE_DIFF(CURRENT_DATE(), c.birth_date, YEAR) AS age,
-
-    MIN(o.order_date) AS first_order_date,
-    MAX(o.order_date) AS last_order_date,
-    COUNT(DISTINCT o.order_id) AS total_orders,
-    SUM(oi.subtotal) AS total_spent
-
-FROM {{ ref('silver_customers') }} as c
-LEFT JOIN {{ ref('silver_orders') }} as o USING(customer_id)
-LEFT JOIN {{ ref('silver_order_items') }} as oi USING(order_id)
-
-WHERE o.status = 'completed'
-GROUP BY 1,2,3,4,5
+    customer_id,
+    name,
+    gender,
+    city,
+    birth_date
+FROM {{ ref('stg_customers') }}
---
+++ transforms/models/gold/fct_customers_metrics.sql
@@ -0,0 +1,10 @@
+{{ config(materialized='table') }}
+
+SELECT
+    customer_id,
+    COUNT(DISTINCT order_id) AS total_orders,
+    SUM(net_revenue) AS total_spent,
+    MIN(order_date) AS first_order_date,
+    MAX(order_date) AS last_order_date
+FROM {{ ref('fact_sales') }}
+GROUP BY customer_id
